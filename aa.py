ac6e9ee25c9d3ac9a771ade632a1631ec90df21f
Othmaneelmo <oelmo031@uottawa.ca>
2025-12-30 06:45:32 -0500

    Modify sleep durations in automation script
    Adjusted sleep durations for better timing in automation.

--- aa.py
+++ aa.py
@@ -1,2 +1,29 @@
 import pyautogui
 import time
+
+# Step 4: Switch to second tab (description)
+time.sleep(3)
+pyautogui.hotkey('ctrl', '2')
+time.sleep(0.5)
+#changed from 5 to 1.5
+time.sleep(3)
+
+# Step 5: Scroll and select description text
+pyautogui.hotkey('ctrl', 'end')
+time.sleep(0.3)
+pyautogui.press('pageup')
+time.sleep(0.3)
+pyautogui.moveTo(1435, 464, duration=0.5)
+time.sleep(0.3)
+pyautogui.mouseDown()
+pyautogui.moveTo(1435, 91, duration=0.5)
+#changed from 3 to 1
+time.sleep(1)
+pyautogui.moveTo(1435, 306, duration=0.3)
+time.sleep(0.3)
+pyautogui.mouseUp()
+pyautogui.hotkey('ctrl', 'c')
+print("Copied from browser.")
+time.sleep(0.3)
+pyautogui.click(button='left')
+time.sleep(0.3)
